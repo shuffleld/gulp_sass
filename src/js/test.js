890c67570309998cc314d6815dbc9296cd418e86
shuffleldliu <shuffleldliu@sohu-inc.com>
2016-04-14 14:56:09 +0800

    add new html

--- src/js/test.js
+++ src/js/test.js
@@ -1,3 +1,26 @@
 $("#div1").on('click',function(){
 	alert("asdads");
 })
+
+class fly{
+	constructor(){}
+			gogo(){
+				$("#touch_me").on('mouseover',function(event) {
+					$(".dialog").css('display','block');
+				})
+				$("#touch_me").on('mouseout',function(event) {
+					$(".dialog").css('display','none');
+				})
+				return this
+			}
+			arrt(arr){  
+				var arr2 = arr;
+				arr2.forEach(function (item, index, array) {
+				  console.log(item, index);
+				});
+				return this
+			}
+		}
+var arr = ['aa','bb','cc','dd','ee','ff','gg',]
+new fly().gogo()
+		.arrt(arr);
